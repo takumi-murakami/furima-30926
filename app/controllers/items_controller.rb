a9ed9356f9cad44092454e2f976d7935b72db50a
takumi-murakami <takumi28280503@gmail.com>
2020-10-15 16:51:17 +0900

    activestrageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+  end
+
+  private
+  def item_params
+  params.require(:item).permit(:title, :text, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :genre_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
