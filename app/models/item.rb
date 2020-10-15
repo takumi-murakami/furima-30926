e0355a5db669732f2a4e8ac4a36d9d43711f800e
takumi-murakami <takumi28280503@gmail.com>
2020-10-15 19:17:57 +0900

    validationの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,5 +9,11 @@ class Item < ApplicationRecord
   belongs_to_active_hash :scheduled_delivery
   # has_one :order
 
-  
+  with_options presence: true do
+    validates :title, :text, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery
+
+    with_options format: {with: /\A[0-9]+\z/} do
+      validates :price, length: { in: (300..9999999)}
+    end
+  end
 end
