c1b6daa2b092789d928431e35418ad5f9ec762f9
takumi-murakami <takumi28280503@gmail.com>
2020-10-15 14:57:15 +0900

    メールアドレスは@を含む必要があること追加テスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,6 +24,11 @@ describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
+      it "emailに@が含まれていない場合登録できない" do
+      @user.email = 'testgmail.com'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email is invalid")
+      end
       it "重複したemailが存在する場合登録できない" do
         @user.save
         another_user = FactoryBot.build(:user)
