2dc61f534e86bb146a3bb0739b5aa035aafb08e3
takumi-murakami <takumi28280503@gmail.com>
2020-10-16 16:46:55 +0900

    修正コミット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,11 +10,12 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
-    end
+      if @item.valid?
+        @item.save
+        redirect_to root_path
+      else
+        render :new
+      end
   end
 
   private
