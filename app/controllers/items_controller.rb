2e63639fc1078c10f85bec5c3c9dbd28a019e507
takumi-murakami <takumi28280503@gmail.com>
2020-10-18 15:27:58 +0900

    修正コミット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,7 +39,8 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    if item.destroy
+    if user_signed_in? && current_user.id == item.user_id
+      item.destroy
       redirect_to root_path
     end
   end
