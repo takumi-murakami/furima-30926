df1aa1830e009423aede9585bb35d8091fa3d6be
takumi-murakami <takumi28280503@gmail.com>
2020-10-18 15:32:25 +0900

    修正コミット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   
   def index
     @items = Item.all.order(created_at: "DESC")
@@ -38,9 +38,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if user_signed_in? && current_user.id == item.user_id
-      item.destroy
+    if user_signed_in? && current_user.id == @item.user_id
+      @item.destroy
       redirect_to root_path
     end
   end
