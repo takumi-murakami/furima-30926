aa25ad6bf0cfb6ab67d892c7b3016a7331c985de
takumi-murakami <takumi28280503@gmail.com>
2020-10-18 14:55:19 +0900

    修正コミット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,12 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
   
   def index
     @items = Item.all.order(created_at: "DESC")
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def new
@@ -24,24 +24,24 @@ class ItemsController < ApplicationController
   end
 
   def edit
-  @item = Item.find(params[:id])
     unless user_signed_in? && current_user.id == @item.user_id
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.valid?
-      @item.save
-      redirect_to item_path(item.id)
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
     else
       render :edit
     end
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
   params.require(:item).permit(:title, :text, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image
