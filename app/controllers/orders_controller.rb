206f3102fb3e593b413062b1f98cb879dfcb8ab1
takumi-murakami <takumi28280503@gmail.com>
2020-10-20 14:35:08 +0900

    payjpの導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,31 +2,35 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @item = ItemOrder.find(params[:id])
+    @item = Item.find(params[:item_id])
     @item_order = ItemOrder.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @item_order = ItemOrder.new(order_params)
+    # binding.pry
     if @item_order.valid?
-      # pay_item
+      pay_item
       @item_order.save
-      redirect_to root_path
+      return redirect_to root_path
     else
       render action: :index
     end
   end
 
   def order_params
-    params.require(:item_order).permit(:postal_code, :prefecture_id, :city_name, :block_name, :building_name, :phone_nember).merge(token: params[:token])
+    binding.pry
+    params.require(:item_order).permit(:postal_code, :prefecture_id, :city_name, :block_name, :building_name, :phone_number).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id )
   end
 
-  # def pay_item
-  #   Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-  #     Payjp::Charge.create(
-  #       amount: order_params[:price],  # 商品の値段
-  #       card: order_params[:token],    # カードトークン
-  #       currency: 'jpy'                 # 通貨の種類（日本円）
-  #     )
-  #   end
+  def pay_item
+    Payjp.api_key = "sk_test_521db06cf645b390232a69e1"
+      Payjp::Charge.create(
+        amount: @item.price,  
+        card: order_params[:token],    
+        currency: 'jpy'                 
+      )
+    end
 end
+
--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,31 +1,17 @@
 class ItemOrder
   include ActiveModel::Model
-  attr_accessor :token, :postal_code, :prefecture_id, :city_name, :block_name, :building_name, :phone_number
-  # ここにitemカラムも追加？
-  # :title, :price, :image
+  attr_accessor :token, :postal_code, :prefecture_id, :city_name, :block_name, :building_name, :phone_number, :item_id, :user_id
 
-  with_options presense: true do
+  with_options presence: true do
     validates :token, :prefecture_id, :city_name, :block_name
-    validates :postal_code, format: { with: /\A[-]?[0-9]+(\.[0-9]+)?\z/ }, length: { is: 8 }
-    validates :phone_nember, format: { with: /\A[0-9]+\z/ }, length: { is: 11 }
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :phone_number, format: { with: /\A\d{11}\z/ }
   end
-# 以下itemモデルのvalidation
-    # with_options presence: true do
-    #   validates :title, :text, :image
-      
-    #   with_options numericality: { other_than: 1 } do
-    #     validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id
-    #   end
-  
-    #   with_options format: { with: /\A[0-9]+\z/ } do
-    #     validates :price, numericality: { greater_than: 300, less_than: 9999999 } 
-    #   end
-    # end
 
     def save
       # item = Item.create(title: title, price: price, image: image)
       Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city_name: city_name, block_name: block_name, building_name: building_name, phone_number: phone_number)
-      Order.create(user_id: user.id, item_id: item.id)
+      Order.create(user_id: user_id, item_id: item_id)
     end
 end
 
