a81727907983f2bca7fad25f37424487704c1650
takumi-murakami <takumi28280503@gmail.com>
2020-10-20 16:54:09 +0900

    soldout実装

--- db/migrate/20201019084135_create_orders.rb
+++ db/migrate/20201019084135_create_orders.rb
@@ -4,7 +4,7 @@ class CreateOrders < ActiveRecord::Migration[6.0]
 
       t.timestamps
       t.references :user, null: false, foreign_key: true
-      t.references :item, null:false, foreign_key: true
+      t.references :item, null: false, foreign_key: true
     end
   end
 end
